3c3181ad9aa21bdce179d270922bb1456db1a730
bilalkhanio <bilalkhan301dir@gmail.com>
2024-11-14 02:12:35 +0500

    updated user slice

--- frontend/src/redux/features/userSlice.js
+++ frontend/src/redux/features/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import api from '../../utils/axios';
+import api from '../../config/api';
 
 // Thunk actions
 export const fetchUsers = createAsyncThunk(
